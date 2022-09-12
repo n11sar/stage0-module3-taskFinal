ddaefb7c6c0b49d3f808a3ca83586c311ee9906d
georgy <ganus1111@gmail.com>
2022-09-13 00:12:02 +0300

    add task solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int firstDigit = number / 1000;
+        int secondDigit = (number - (firstDigit * 1000)) / 100;
+        int thirdDigit = (number - firstDigit * 1000 - secondDigit * 100) / 10;
+        int fourthDigit = number % 10;
+
+        int res = firstDigit + secondDigit + thirdDigit + fourthDigit;
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int firstDigit = number / 100;
+        int secondDigit = (number - (100 * firstDigit)) / 10;
+        int thirdDigit = (number - (firstDigit * 100)) % 10;
+
+        int result = thirdDigit * 100 + secondDigit * 10 + firstDigit;
+        System.out.println(result);
     }
 }
